e1f9ef827ace86dc3b978844ff9522928bfd363a
dyh210312 <dyh210312@gmail.com>
2026-03-30 00:55:37 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- lexsem-project/setupA_cloze/cloze_zero_shot/cloze_zero_shot/Methods/evaluate_h1_all_relations.py
+++ lexsem-project/setupA_cloze/cloze_zero_shot/cloze_zero_shot/Methods/evaluate_h1_all_relations.py
@@ -1,8 +1,9 @@
+import os
+print("Working dir:", os.getcwd())
 import json
 import csv
 from collections import defaultdict
-import os
-print("Working dir:", os.getcwd())
+
 INPUT_FILE = "cloze_val_predictions.jsonl"
 OUTPUT_CSV = "h1_full_relation_results_optimized.csv"
 
